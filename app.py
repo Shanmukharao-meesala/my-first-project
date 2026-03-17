9af4c22cba9ff32140395bdaa953ecd7911c9311
Shanmukharao-meesala <shanmukhmeesala@gmail.comcleargit config --global user.email shanmukhmeesala@gmail.com>
2026-03-17 09:52:35 +0530

    calculator app

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-print("my secound deployment")
-print("DEVOPS is awesome!")
+def add(a, b);
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiply(a, b)
+    return a * b
+
+print("CALCULATOR APP")
+
+print("10 + 5 =", add(10, 5))
+print("10 - 5 +", subtract(10, 5))
+print("10 * 5 +", multiply(10, 5))
