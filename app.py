0f931d129a8ff25b04b0023e8699d1e6b70b6100
Shanmukharao-meesala <shanmukhmeesala@gmail.comcleargit config --global user.email shanmukhmeesala@gmail.com>
2026-03-17 10:23:43 +0530

    calculator app

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-def add(a, b);
-    return a + b
+def add(a, b):
+     return a + b
 
 def subtraction(a, b):
-    return a - b
+     return a - b
 
-def multiply(a, b)
-    return a * b
+def multiply(a, b):
+     return a * b
 
 print("CALCULATOR APP")
 
-print("10 + 5 =", add(10, 5))
-print("10 - 5 +", subtract(10, 5))
-print("10 * 5 +", multiply(10, 5))
+a = int(input("Enter first number: "))
+b = int(input("Enter secound number: "))
+
+print("addition:", add(a, b))
+print("subtraction", subtraction(a, b))
+print("multiplication", multiply(a, b))
