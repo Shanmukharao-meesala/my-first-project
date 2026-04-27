c94730b280338a28c3b356b681fedd619895af89
Shanmukharao-meesala <shanmukhmeesala@gmail.com>
2026-04-27 01:18:36 +0000

    add pytest testing to CI/CD pipeline

--- test_app.py
+++ test_app.py
@@ -1,8 +1,17 @@
-from app import students
+from app import generate_ticket
 
-def test_student_narks():
-    for student in students:
-        assert student["marks"] >= 0
-        assert student["marks"] <= 100
-    print("ALL TEST PASSED!")
+def test_ticket_has_3_rows():
+    ticket = generate_ticket()
+    assert len(ticket) == 3
 
+def test_ticket_numbers_in_range():
+    ticket = generate_ticket()
+    for row in ticket:
+        for num in row:
+            if num != 0:
+                assert 1 <= num <= 90
+
+def test_ticket_has_9_columns():
+    ticket = generate_ticket()
+    for row in ticket:
+        assert len(row) == 9
